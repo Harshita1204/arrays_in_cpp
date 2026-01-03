dca80aacc32b8dc22756a4eccad462816e759e11
Harshita1204 <harshitak1204@gmail.com>
2026-01-03 22:57:42 +0530

    Create simple_array.cpp to demonstrate array usage
    Added a simple C++ program to demonstrate array declaration and access.

---
+++ simple_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+    //declare and array
+    int number[15];
+    
+    //accessing an array
+     cout<<"value at index o"<<number[0]<<endl;
+     cout<<"value at index 9"<<number[9]<<endl;
+     cout<<"value at index 11"<<number[11]<<endl;
+     // in output there will be a garbage value because we have not given any value yet!
+    return 0;
+}
